29547f08475f9f2ccfcf009acad886552ca107ce
magnetic-quiz <magneticquiz@gmail.com>
2025-03-13 06:49:16 +0530

    dot env file added

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express, { json } from "express";
 import { connect } from "mongoose";
 import userRoutes from "./routes/user.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(json());
 app.use("/api/users", userRoutes);
 
-connect(
-  "mongodb+srv://magneticquiz:zvD1mjrOcyyagiwd@magneticcluster.ad5u1.mongodb.net/userbase?retryWrites=true&w=majority&appName=magneticCluster"
-).then(() => {
-  app.listen(5001, () => {
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+connect(MONGO_URI).then(() => {
+  app.listen(PORT, () => {
     console.log("server running at port 5001");
   });
   console.log("Database Connected!");
